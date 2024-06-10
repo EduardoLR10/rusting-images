cf258152aa7a8bc66b80f95616970cdacd6d36d8
EduardoLR10 <dudulr10@gmail.com>
2024-06-09 22:41:07 -0300

    Move from crate2nix to cargo2nix

--- flake.nix
+++ flake.nix
@@ -1,39 +1,53 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    crate2nix = {
-      url = "github:kolloch/crate2nix";
-      flake = false;
-    };
-    flake-utils.url = "github:numtide/flake-utils";
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
   };
 
-  outputs = { self, nixpkgs, crate2nix, flake-utils }:
+  outputs = inputs: with inputs; # pass through all inputs and bring them into scope
+
+    # Build the output set for each default system and map system sets into
+    # attributes, resulting in paths such as:
+    # nix build .#packages.x86_64-linux.<name>
     flake-utils.lib.eachDefaultSystem (system:
+
+      # let-in expressions, very similar to Rust's let bindings.  These names
+      # are used to express the output but not themselves paths in the output.
       let
-        pkgs = nixpkgs.legacyPackages.${system};
 
-        crateName = "rusting-images";
-
-        inherit (import "${crate2nix}/tools.nix" { inherit pkgs; })
-          generatedCargoNix;
-
-        project = import (generatedCargoNix {
-          name = crateName;
-          src = ./.;
-        }) {
-          inherit pkgs;
-          defaultCrateOverrides = pkgs.defaultCrateOverrides // {};
+        # create nixpkgs that contains rustBuilder from cargo2nix overlay
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ cargo2nix.overlays.default ];
         };
 
-      in {
-        packages.${crateName} = project.rootCrate.build;
+        # create the workspace & dependencies package set
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          rustVersion = "1.77.0";
+          packageFun = import ./Cargo.nix;
+        };
 
-        defaultPackage = self.packages.${system}.${crateName};
+      in rec {
+        # this is the output (recursive) set (expressed for each system)
+
+        # the packages in `nix build .#packages.<system>.<name>`
+        packages = {
+          # nix build .#rusting-images
+          # nix build .#packages.x86_64-linux.rusting-images
+          rusting-images = (rustPkgs.workspace.rusting-images {});
+          # nix build
+          default = packages.rusting-images; # rec
+        };
 
         devShell = pkgs.mkShell {
-          inputsFrom = builtins.attrValues self.packages.${system};
-          buildInputs = [ pkgs.cargo pkgs.rust-analyzer pkgs.clippy ];
+          buildInputs = with pkgs [
+            cargo
+            rust-analyzer
+            clippy
+            just
+          ];
         };
-      });
+      }
+    );
 }
